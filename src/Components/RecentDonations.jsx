334cc11021097cd27fc1af173e266c78e6dadcb0
Erick Tolentino <ericktolentino@pursuit.org>
2023-07-30 11:56:14 -0400

    finished the lab

--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,26 @@
-export default function RecentDonations() {
-  return null;
+import React from "react";
+export default function RecentDonations({donations}) {
+
+  
+
+  const listDonations = donations.map((donation) => {
+    return (
+      <>
+      <li>
+        <span>{donation.name} donated ${donation.amount}</span>{donation.caption}
+      </li>
+      </>
+    )
+  })
+
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        
+        {listDonations}
+         
+      </ul>
+    </section>
+  );
 }
